cac82e0228d8aa5cb32933890f06e0c28f225bde
Pronin Alexandr <a.pronin.kem@gmail.com>
2026-09-24 20:08:23 +0700

    Выполнена последовательность Фибоначчи

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -13,3 +13,19 @@ static long Factorial(int n)
         return result;
     
 }
+int result1 = 0;
+int result2 = 1;
+Console.WriteLine("Введите последовательность:");
+if (!int.TryParse(Console.ReadLine(), out int numbers))
+{
+    Console.WriteLine($"Вы ввели: {numbers}");
+}
+for (int i=0;i<=numbers;i++)
+{
+    int result3 = result1 + result2;
+    result1 = result2;
+    result2 = result3;
+
+    Console.WriteLine($"{result3}");
+}
+Console.WriteLine($"Итог последовательности Фибоначчи приведён выше с колчиепство шагов равным:{numbers}");
