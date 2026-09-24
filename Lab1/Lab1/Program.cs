80c42f12f570161595f6896788b8a2b8f6e56d09
Pronin Alexandr <a.pronin.kem@gmail.com>
2026-09-24 19:10:02 +0700

    Создана ветка для первой лабораторной

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите n от 0 до 20:");
+if (!int.TryParse(Console.ReadLine(),out int n)||n<0||n>20)
+{
+    Console.WriteLine("Ошибка: Введите ЦЕЛОЕ число от ");
+    return;
+}
+Console.WriteLine($"{n}!={Factorial(n)}");
+static long Factorial(int n)
+{
+    long result = 1;
+    for(int i=2;i<=n;i++)
+        result *= i;
+        return result;
+    
+}
